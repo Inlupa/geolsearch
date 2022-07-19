30db9af59b1d4e3c408669cdc8578e905bc637b1
Inlupa <52817215+Inlupa@users.noreply.github.com>
2022-07-19 12:55:25 +0300

    delete key

--- road_rework/road_rework/settings.py
+++ road_rework/road_rework/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nv*#=&(bhom@w=aic5%fuo2_7s)z3^obgr(q(b3g3m7jy&v@)p'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
